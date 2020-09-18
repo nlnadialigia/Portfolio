b017a21350ec515170a59299800b8c17adb8d8be
Nadia Ligia <nlnadialigia@gmail.com>
2020-09-17 21:02:19 -0300

    iniciando server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.use(express.static('public'))
+
+server.set('view engine', 'njk')
+
+nunjucks.configure('views', {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+server.listen(3020, ()=>{
+    console.log('SERVER IS RUNNING');
+})
