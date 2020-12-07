6c6585a2e624908d5d77fbe9cbd0e26467091295
Nadia Ligia <nlnadialigia@gmail.com>
2020-12-06 22:24:45 -0300

    Footer update

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,11 +1,10 @@
-const contact = document.getElementById('menu')
-const social = document.getElementById('social')
+const contact = document.getElementById('menu');
+const social = document.getElementById('social');
 
-contact.addEventListener('click', ()=>{
-   if (social.style.display == 'flex') {
-       social.style.display = 'none'
-   } else {
-       social.style.display = 'flex'
-   }
-    
-})
+contact.addEventListener('click', () => {
+  if (social.style.display === 'flex') {
+    social.style.display = 'none';
+  } else {
+    social.style.display = 'flex';
+  }
+});
